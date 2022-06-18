4f0d8768663d46f358b48ff16cda5782953b0124
carloscarrete <carretecarlosg@gmail.com>
2022-06-18 15:28:48 -0500

    minour changes on index.html abd auth.controller

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -4,16 +4,22 @@ const { generateToken } = require("../utils/manageToken");
 const login = ('/login', async (req, res) => {
 
     const { email, password } = req.body;
-    try{
+    try {
         let user = await User.findOne({ email });
-        if(!user) return res.status(403).json({ message: 'Las credenciales no coiciden' });
+        if (!user) return res.status(403).json({ message: 'Las credenciales no coiciden' });
         const confirmPassword = await user.comparePasword(password);
-        if(!confirmPassword) return res.status(403).json({ message: 'Las credenciales no coiciden' });
+        if (!confirmPassword) return res.status(403).json({ message: 'Las credenciales no coiciden' });
 
-        const {token, expiresIn} = generateToken(user.id);
+        const { token, expiresIn } = generateToken(user.id);
         console.log(token, expiresIn);
 
-    }catch(error){
+        return res.status(200).json({
+            ok: true,
+            token,
+            expiresIn
+        })
+
+    } catch (error) {
         console.log(error);
         res.status(400).json({
             ok: false,
@@ -21,33 +27,33 @@ const login = ('/login', async (req, res) => {
         })
     }
 
-    res.status(200).json({
+    return res.status(200).json({
         ok: true,
         message: 'Login Successful',
         name: req.body.name,
     })
 });
 
-const register = ('/register', async (req, res) =>{
+const register = ('/register', async (req, res) => {
     const { email, password } = req.body;
-     try{
-        let user = await User.findOne({email});
-        if(user) throw new Error('El usuario ya existe');
-        user = new User({email, password});
+    try {
+        let user = await User.findOne({ email });
+        if (user) throw new Error('El usuario ya existe');
+        user = new User({ email, password });
         await user.save();
-        return res.status(201).json({ok: true});
-    }catch(error){
+        return res.status(201).json({ ok: true });
+    } catch (error) {
         console.log(error);
         res.status(400).json({
             ok: false,
             message: error.message
         })
-    } 
+    }
 
 })
 
-const infoUser=async(req,res)=>{
-    try{
+const infoUser = async (req, res) => {
+    try {
         //Con lean es para pasarlo un objeto simple y que la consulta sea más rápida
         const user = await User.findById(req.uid).lean();
         res.json({
@@ -55,7 +61,7 @@ const infoUser=async(req,res)=>{
             email: user.email,
             id: user._id
         });
-    }catch(error){
+    } catch (error) {
         console.log(error);
         res.status(400).json({
             ok: false,
@@ -65,6 +71,6 @@ const infoUser=async(req,res)=>{
 };
 
 
-module.exports = {login, register, infoUser};
+module.exports = { login, register, infoUser };
 
 
